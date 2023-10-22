91b3da6a15445b1ab5ea0e0910588ff08723f448
anthony-8 <anthony_liang@student.uml.edu>
2023-10-21 20:21:43 -0400

    First commit

---
+++ script.js
@@ -0,0 +1,43 @@
+const tbody = document.getElementById("tbody");
+const thead = document.getElementById("thead");
+
+function getForm() {
+  const form = document.getElementById("form");
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    // save values
+    var min_col_val = parseInt(document.getElementById("min_col_val").value);
+    var max_col_val = parseInt(document.getElementById("max_col_val").value);
+    var min_row_val = parseInt(document.getElementById("min_row_val").value);
+    var max_row_val = parseInt(document.getElementById("max_row_val").value);
+
+    tbody.innerHTML = "";
+    thead.innerHTML = "";
+
+    console.log(min_col_val);
+
+    const trHeader = document.createElement("tr");
+    for (var col = min_col_val; col <= max_col_val; col++) {
+      const th = document.createElement("th");
+      th.innerText = col;
+      trHeader.appendChild(th);
+    }
+    thead.appendChild(trHeader);
+
+    for (var row = min_row_val; row <= max_row_val; row++) {
+        const trData = document.createElement("tr");
+        for (var col = min_col_val; col <= max_col_val; col++) {
+          const td = document.createElement("td");
+          // Calculate and display the result in the cell
+          const result = row * col;
+          td.innerText = result;
+          trData.appendChild(td);
+        }
+        tbody.appendChild(trData);
+      }
+
+    form.reset();
+  });
+}
+
+getForm();
